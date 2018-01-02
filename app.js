37759e584d1291cf65ba2380acd308e61e4c8dd9
Aaron Franco <aarongfranco@gmail.com>
2018-01-02 08:45:49 -0500

    Super simple server

---
+++ app.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const Hapi = require('hapi');
+const AWS = require('aws-sdk');
+
+// Create a server with a host and port
+const server = Hapi.server({
+    host: 'localhost',
+    port: 8001
+});
+
+// Add the route
+server.route({
+    method: 'GET',
+    path:'/jane',
+    handler: function (request, h) {
+
+        return {
+             "username":"jane",
+             "name":"Jane Doe",
+             "music":[
+                  {
+                       "title":"Track One",
+                       "file":'414365__erokia__ambient-wave-28.wav'
+                  },
+                  {
+                       "title":"Track Two",
+                       "file":"414497__toiletrolltube__hellocatfood-b6b7-o.wav"
+                  },
+                  {
+                       'title':"Track Three",
+                       "file":'414514__eardeer__daybreak.wav'
+                  }
+             ]
+        }
+   
+    }
+});
+
+// Start the server
+async function start() {
+
+    try {
+        await server.start();
+    }
+    catch (err) {
+        console.log(err);
+        process.exit(1);
+    }
+
+    console.log('Server running at:', server.info.uri);
+};
+
+start();
